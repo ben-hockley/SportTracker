0d8c7a1b8a575c362afc7579406521838d3aab61
Ben Hockley <hockleyb@cardiff.ac.uk>
2024-12-16 18:30:15 +0000

    spring initializr

---
+++ src/main/java/com/SportTracker/SportTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.SportTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTrackerApplication.class, args);
+	}
+
+}
