25f096e55bbd789a7a83036edac7bf6c16b49ee4
Ben Hockley <hockleyb@cardiff.ac.uk>
2024-12-20 12:08:58 +0000

    rearranged new game form with bootstrap

--- src/main/resources/static/JS/allPlayers.js
+++ src/main/resources/static/JS/allPlayers.js
@@ -1,12 +1,26 @@
-function filterByTeam() {
+// Search (JQuery) ; credit W3Schools
+$(document).ready(function(){
+    $("#search").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#playerTableBody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+
+
+function filter() {
+    var position = document.getElementById("position").value;
     var team = document.getElementById("team").value;
     var table = document.getElementById("playerTable");
     var tr = table.getElementsByTagName("tr");
     for (var i = 1; i < tr.length; i++) {
         var teamName = tr[i].getElementsByTagName("td")[1].getElementsByTagName("a")[0].innerHTML;
-        if (teamName != null) {
-            console.log(teamName);
-            if (teamName == team || team == "All") {
+        var positionName = tr[i].getElementsByTagName("td")[2].innerHTML;
+        if (positionName != null) {
+            console.log(positionName);
+            if ((positionName == position || position == "All") && (teamName == team || team == "All")) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
