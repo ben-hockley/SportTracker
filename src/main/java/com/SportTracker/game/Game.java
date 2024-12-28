9d00e901a6ba69dbb080b58b03a2f32a903810d0
Ben Hockley <hockleyb@cardiff.ac.uk>
2024-12-28 10:59:48 +0000

    fixed stat view in league details

--- src/main/java/com/SportTracker/game/Game.java
+++ src/main/java/com/SportTracker/game/Game.java
@@ -1,12 +1,11 @@
 package com.SportTracker.game;
 
+import java.sql.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-
-import java.sql.Date;
 
 @Getter
 @Setter
